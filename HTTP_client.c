20f99f4768ed772e5959127647536220d53273a8
EastRoot Jang <55939719+Logic-01001010@users.noreply.github.com>
2021-07-16 20:03:50 +0900

    Update HTTP_client.c

--- HTTP_client.c
+++ HTTP_client.c
@@ -17,7 +17,7 @@ int main(int argc, char* argv[])
     
     if(argc != 3)
     {
-        printf("Usasge : %s <IP> <port>\n", argv[0]);
+        printf("Usage : %s <IP> <port>\n", argv[0]);
         exit(1);
     }
 
